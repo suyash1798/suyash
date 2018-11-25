0519a92c4992b8bc1606857affaa1edd4fd746b8
suyash1798 <suyashtiwari1798@gmail.com>
2018-11-25 14:55:58 +0530

    completed

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+$(window).on("load",function () {
+   $(".loader .inner").fadeOut(500, function () {
+       $(".loader").fadeOut(750)
+   }) ;
+
+   $("items").fadeOut({
+       filter: '*',
+       animationOptions:{
+           duration:1500,
+           easing:'linear',
+           queue:false
+       }
+   })
+});
+
 $(document).ready(function () {
    $('#slides').superslides({
        animation:'fade',
@@ -65,5 +80,57 @@ $(document).ready(function () {
     });
 
     $("[data-fancybox]").fancybox();
+
+
+
+
+    $('.ac').click(function (){
+
+        console.log('entered');
+        $('.filters').find('.current').removeClass("current");
+        $(this).addClass("current");
+
+        var selector = $(this).attr("data-filter");
+
+        $(".items").isotope({
+            filter: selector,
+            animationOptions:{
+                duration: 1500,
+                easing: 'linear',
+                queue: false
+            }
+        });
+
+        return false;
+    });
+
+    const nav = $("#navigation");
+
+    // nav.find('a').click(function (e) {
+    //    e.preventDefault();
+    //
+    //    var targetElement = $(this).attr("href");
+    //    var targetPosition = $(targetElement).offset.top;
+    //    $("html, body").animate({ scrollTop: targetPosition - 50}, "slow")
+    // });
+
+    // const nav = $("#navigation");
+    const navTop = nav.offset().top;
+
+    $(window).on("scroll", stickyNavigation);
+
+    function stickyNavigation(){
+        var body = $("body");
+
+        if($(window).scrollTop() >= navTop){
+            body.css("padding-top", nav.outerHeight() + "px")
+            body.addClass("fixedNav");
+
+        }
+        else{
+            body.css("padding-top",0);
+            body.removeClass("fixedNav");
+        }
+    }
 
 });
